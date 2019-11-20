8f6d7176bd023419bc4e02925b8297627ee1f92c
raja <raja.vajjiravelu@apolisrises.com>
2019-11-21 02:16:06 +0530

    top nav, sidebar nav, subscribe, collection area

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -2,44 +2,53 @@
     <ul class="sidebar navbar-nav">
       <li class="nav-item {{ request()->is('home*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ url('home') }}">
-          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <i class="fas fa-chevron-right float-right"></i>
+          <i class="fas fa-tachometer-alt"></i>
+
           <span>Dashboard</span>
         </a>
       </li>
 
       <li class="nav-item {{ request()->is('branch*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ url('branch') }}">
-          <i class="fas fa-fw fa-chart-area"></i>
+          <i class="fas fa-chevron-right float-right"></i>
+          <i class="fas fa-code-branch"></i>
           <span>Branch</span></a>
       </li>
       <li class="nav-item {{ request()->is('subscriber*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ url('subscriber') }}">
-          <i class="fas fa-fw fa-table"></i>
+          <i class="fas fa-chevron-right float-right"></i>
+          <i class="fas fa-exclamation-circle"></i>
           <span>Subscriber</span></a>
       </li>
       <li class="nav-item {{ request()->is('collection-area*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ url('collection-area') }}">
-          <i class="fas fa-fw fa-table"></i>
+          <i class="fas fa-chevron-right float-right"></i>
+          <i class="fas fa-hand-holding-usd"></i>
           <span>Collection-Area</span></a>
       </li>
       <li class="nav-item {{ request()->is('scheme*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ url('scheme') }}">
-          <i class="fas fa-fw fa-table"></i>
+          <i class="fas fa-chevron-right float-right"></i>
+          <i class="far fa-clock"></i>
           <span>Scheme</span></a>
       </li>
       <li class="nav-item {{ request()->is('bank*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ url('bank') }}">
-          <i class="fas fa-fw fa-table"></i>
+          <i class="fas fa-chevron-right float-right"></i>
+          <i class="fas fa-money-check-alt"></i>
           <span>Bank</span></a>
       </li>
       <li class="nav-item {{ request()->is('group*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ url('group') }}">
-          <i class="fas fa-fw fa-table"></i>
+          <i class="fas fa-chevron-right float-right"></i>
+          <i class="fas fa-users"></i>
           <span>Group</span></a>
       </li>
       <li class="nav-item {{ request()->is('ledger*') ? 'active' : '' }}">
         <a class="nav-link" href="{{ url('ledger') }}">
-          <i class="fas fa-fw fa-table"></i>
+          <i class="fas fa-chevron-right float-right"></i>
+          <i class="fas fa-clipboard-list"></i>
           <span>Ledger</span></a>
       </li>
       
