6395c37110b6a0042ef04b60fd27079321297cd5
mscsoft04 <mscsoft04@gmail.com>
2019-12-17 04:33:34 +0530

    leger group view change

--- resources/views/ledger/show_group.blade.php
+++ resources/views/ledger/show_group.blade.php
@@ -23,7 +23,6 @@
 			@else
 			<td><a href="javascript:void(0)" class="add-payment" data-id="{{$row[0] }}">{{ $row[0]->group_name }}</a></td>
 			@endif
-		   <td><a class="add-payment" data-id="{{$row[0] }}">{{ $row[0]->group_name }}</a></td>
 			<td>{{ $row[0]->ticket_number }}</td>
 			<td>{{ $row[0]->collection_type }}</td>
 			<td>{{ $row[0]->chit_value }}</td>
