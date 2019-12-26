3b788497b529e1a280fd9fd1dd7df1e6c4a161fd
mscsoft04 <mscsoft04@gmail.com>
2019-12-27 04:08:02 +0530

    payment add view design integration

--- public/js/sb-admin.js
+++ public/js/sb-admin.js
@@ -93,7 +93,8 @@ $("#sidebarToggle").on('click', function(e) {
   });
 
   // Add the following code if you want the name of the file appear on select
-$(".custom-file-input").on("change", function() {
+
+$(document).on("change", ".custom-file-input", function() {
   var fileName = $(this).val().split("\\").pop();
   $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
 });
